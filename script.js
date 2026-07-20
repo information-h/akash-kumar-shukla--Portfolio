c9c531703662027e66559aa855a353a674a6a123
information-h <akashkumarshuk805133@gmail.com>
2026-07-20 11:52:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,22 @@ document.querySelectorAll('a[href^="#"]')
   .forEach(a=>a.onclick=e=>{e.preventDefault();
       document.querySelector(a.getAttribute('href')).
         scrollIntoView({behavior:'smooth'})});
-// code fragment
-// the form id is myForm
 $('#myForm').on('submit', function(event) {
-    event.preventDefault(); // prevent reload
-    
+    event.preventDefault();
+
     var formData = new FormData(this);
     formData.append('service_id', 'YOUR_SERVICE_ID');
     formData.append('template_id', 'YOUR_TEMPLATE_ID');
     formData.append('user_id', 'YOUR_PUBLIC_KEY');
- 
+
     $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
         type: 'POST',
         data: formData,
-        contentType: false, // auto-detection
-        processData: false // no need to parse formData to string
+        contentType: false,
+        processData: false
     }).done(function() {
         alert('Your mail is sent!');
     }).fail(function(error) {
         alert('Oops... ' + JSON.stringify(error));
     });
 });
-// code fragment
